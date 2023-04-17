de6e591fbcc1d4e339354676af474fac8925f065
SimchaTeich <sim.teich@gmail.com>
2023-04-17 19:43:41 +0300

    create class Fraction with two members and C'TOR

---
+++ sources/Fraction.hpp
@@ -0,0 +1,17 @@
+#ifndef FRACTION_HPP
+#define FRACTION_HPP
+
+
+class Fraction
+{
+private:
+    int _numerator;
+    int _denominator;
+
+public:
+    Fraction(const int numerator, const int denominator);
+};
+
+
+
+#endif // !FRACTION_HPP
