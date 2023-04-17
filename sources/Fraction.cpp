7009b7a4f1fe22acb007d839234eceb26e9858c4
SimchaTeich <sim.teich@gmail.com>
2023-04-17 19:50:24 +0300

    implements the C'TOR

---
+++ sources/Fraction.cpp
@@ -0,0 +1,10 @@
+#include "Fraction.hpp"
+
+using namespace ariel;
+
+
+Fraction::Fraction(const int numerator, const int denominator)
+{
+    _numerator = numerator;
+    _denominator = denominator;
+}
