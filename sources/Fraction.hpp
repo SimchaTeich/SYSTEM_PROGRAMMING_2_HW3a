40f6bb2ff42ecafc785ba26c568d239010f694ad
SimchaTeich <sim.teich@gmail.com>
2023-04-17 19:46:56 +0300

    capsulate the class with namespace ariel

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -1,17 +1,17 @@
 #ifndef FRACTION_HPP
 #define FRACTION_HPP
 
-
-class Fraction
+namespace ariel
 {
-private:
-    int _numerator;
-    int _denominator;
+    class Fraction
+    {
+    private:
+        int _numerator;
+        int _denominator;
 
-public:
-    Fraction(const int numerator, const int denominator);
+    public:
+        Fraction(const int numerator, const int denominator);
+    };
 };
-
-
 
 #endif // !FRACTION_HPP
