5d8fa6d5add15da9e6076667b000a028a6b55c4a
SimchaTeich <sim.teich@gmail.com>
2023-04-17 20:01:30 +0300

    implements operator<<

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -1,10 +1,17 @@
 #include "Fraction.hpp"
 
 using namespace ariel;
+using std::endl;
 
 
 Fraction::Fraction(const int numerator, const int denominator)
 {
     _numerator = numerator;
     _denominator = denominator;
+}
+
+
+ostream& operator<<(ostream& s, const Fraction& fraction)
+{
+    s << fraction.numerator() << "/" << fraction.denominator() << endl;
 }
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -1,6 +1,12 @@
 #ifndef FRACTION_HPP
 #define FRACTION_HPP
 
+#include <iostream>
+
+using std::ostream;
+using std::endl;
+
+
 namespace ariel
 {
     class Fraction
@@ -15,6 +21,15 @@ namespace ariel
         input: the integers for numerator and denominator.
         */
         Fraction(const int numerator, const int denominator);
+
+        /*
+        Prints Fraction object into output stream.
+        For example, [cout << Fraction(1,2)] will
+        print: "1\2" into cout stream.
+        input: stream and Fraction object.
+        output: the stream after changes.
+        */
+        friend ostream& operator<<(ostream& s, const Fraction& fraction);
     };
 };
 
